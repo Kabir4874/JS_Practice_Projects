de885669903defe54834b1b79d9c2a1ce0752258
Kabir4874 <kabir.cse.bd@gmail.com>
2024-02-16 16:07:58 +0600

    add

--- Weather Application/index.js
+++ Weather Application/index.js
@@ -1,64 +1,44 @@
-const userTab= document.querySelector("[data-userWeather]");
+const userTab = document.querySelector("[data-userWeather]");
 
-const searchTab= document.querySelector("[data-searchWeather]");
+const searchTab = document.querySelector("[data-searchWeather]");
 
-const userContainer= document.querySelector(".weather-container");
+const userContainer = document.querySelector(".weather-container");
 
-const grantAccessContainer= document.querySelector(".grant-location-container");
+const grantAccessContainer = document.querySelector(
+  ".grant-location-container"
+);
 
-const searchForm= document.querySelector("[data-searchForm]");
+const searchForm = document.querySelector("[data-searchForm]");
 
-const loadingScreen= document.querySelector(".loading-container");
+const loadingScreen = document.querySelector(".loading-container");
 
-const userInfoContainer= document.querySelector(".user-info-container");
+const userInfoContainer = document.querySelector(".user-info-container");
 
-let currentTab= userTab;
-const apiKey= '50645afe7dc713ebcce15941bd98bf95';
+let currentTab = userTab;
+const apiKey = "50645afe7dc713ebcce15941bd98bf95";
 
 currentTab.classList.add("current-tab");
 
+function switchTab(clickedTab) {
+  if (clickedTab != currentTab) {
+    currentTab.classList.remove("current-tab");
+    currentTab = clickedTab;
+    currentTab.classList.add("current-tab");
 
+    if (!searchForm.classList.contains("active")) {
+      userInfoContainer.classList.remove("active");
+      grantAccessContainer.classList.remove("active");
+      searchForm.classList.add("active");
+    }
+  }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+userTab.addEventListener("click", () => {
+  switchTab(userTab);
+});
+searchTab.addEventListener("click", () => {
+  switchTab(searchTab);
+});
 
 // 50645afe7dc713ebcce15941bd98bf95
 
