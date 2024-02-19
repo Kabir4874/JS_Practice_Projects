c0196e866109433461389a5cb830e3d29c63667d
Kabir4874 <kabir.cse.bd@gmail.com>
2024-02-20 00:04:33 +0600

    add

--- Dev Detective/script.js
+++ Dev Detective/script.js
@@ -7,6 +7,15 @@ const form = document.querySelector("[form]");
 const input = document.querySelector("[inputValue]");
 const avatar = document.querySelector("[avatar]");
 const displayName = document.querySelector("[displayName]");
+const login= document.querySelector("[login]");
+const  about= document.querySelector("[about]");
+const repo= document.querySelector("[repo]");
+const followers= document.querySelector("[followers]");
+const following= document.querySelector("[following]");
+const Location = document.querySelector("[location]");
+const link= document.querySelector("[link]");
+const twitter= document.querySelector("[twitter]");
+const company= document.querySelector("[company]");
 
 // !Dark Mode
 function setDarkMode() {
@@ -33,6 +42,13 @@ if (mode === "light") {
 } else {
   setDarkMode();
 }
+
+async function apiCall() {
+    const response = await fetch(`https://api.github.com/users/` + userName);
+    const data = await response.json();
+    console.log(data);
+    render(data);
+  }
 let userName;
 modeBtn.addEventListener("click", toggleMode);
 form.addEventListener("submit", (e) => {
@@ -45,12 +61,14 @@ form.addEventListener("submit", (e) => {
 function render(data) {
   avatar.src = `${data.avatar_url}`;
   displayName.innerText = `${data.name}`;
+  login.innerText= `@${data.login}`;
+  about.innerText= `${data.bio}`;
+  repo.innerText=`${data.public_repos}`;
+  followers.innerText=`${data.followers}`;
+  following.innerText= `${data.following}`;
+  Location.innerText= `${data.location}`;
+  company.innerText=`${data.company}`;
 }
 
-async function apiCall() {
-  const response = await fetch(`https://api.github.com/users/` + userName);
-  const data = await response.json();
-  console.log(data);
-  render(data);
-}
+
 // const url = "https://api.github.com/users/";
